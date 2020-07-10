5071f35152f1f7ef6a7716c315654c625a364c93
szeyuhninawang <szeyuhnina.wang@mail.utoronto.ca>
2020-07-10 11:55:34 -0400

    Updated analysis code and added plot code

---
+++ Congress/congress_moral_language.R
@@ -0,0 +1,240 @@
+#### REQUIREMENTS ####
+library(RSQLite)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(data.table)
+
+library(lme4)
+library(lmerTest)
+library(optimx)
+library(car)
+library(stats)
+
+library(here)
+
+#### DATA ####
+
+# WARNING: If data are not present locally, this will download a ~966 MB file - don't run over a metered connection
+data <- NULL
+# try to read local copy
+try( data <- fread( here('congress_data.csv'), data.table = FALSE ), silent = TRUE )
+
+if( is.null(data) ) {
+  # local read failed, get it remotely
+  data <- fread("https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/FQ8MIL/E2WK6A", 
+                  data.table = FALSE )
+  # save for future runs
+  fwrite(data, file = here('congress_data.csv') )
+}
+
+
+##### MODELLING #####
+
+# Define list of foundations
+foundations <- c( "Carevirtue", "Carevice", "Fairnessvirtue", "Fairnessvice", "Loyaltyvirtue", "Loyaltyvice", "Authorityvirtue",
+                  "Authorityvice", "Purityvirtue",  "Purityvice" )
+
+# Average for each speaker across each Congress and chamber in order to get these models to converge (chamber - to account for people who switch chambers within a Congress)
+bycongress.speaker <- data %>% group_by(congress, speaker, chamber) %>% summarize(
+  Carevirtue = mean(Carevirtue),
+  Carevice   = mean(Carevice),
+  
+  Fairnessvirtue = mean(Fairnessvirtue),
+  Fairnessvice = mean(Fairnessvice),
+  
+  Loyaltyvirtue = mean(Loyaltyvirtue),
+  Loyaltyvice = mean(Loyaltyvice),
+  
+  Authorityvirtue = mean(Authorityvirtue),
+  Authorityvice = mean(Authorityvice),
+  
+  Purityvirtue = mean(Purityvirtue),
+  Purityvice = mean(Purityvice),
+  
+  senate_control = unique(senate_control),
+  house_control = unique(house_control),
+  President = unique(President),
+  party = unique(party)
+)
+
+## Data clean-up: remove non-voting members; fix party affiliations for Independents ##
+# remove: 11345,	10662, 12118, 11118, 11343
+# change to D: 	10402, 11181, 12229, 11647, 11822, 11310 (107-109th Congress)
+# change to R: 	11909, 11483 (in 106th Congress)
+bycongress.speaker <- bycongress.speaker %>% 
+  filter(!(speaker %in% c("11345", "10662", "12118", "11118", "11343")))
+bycongress.speaker$party <- ifelse(bycongress.speaker$speaker %in% c("10402", "11181", "12229", "11647", "11822"), "D", bycongress.speaker$party)
+bycongress.speaker$party <- ifelse(bycongress.speaker$speaker == "11909", "R", bycongress.speaker$party)
+bycongress.speaker$party <- ifelse(bycongress.speaker$speaker == "11310" & bycongress.speaker$congress >= 107 & bycongress.speaker$congress <= 109, "D", bycongress.speaker$party)
+bycongress.speaker$party <- ifelse(bycongress.speaker$speaker == "11483" & bycongress.speaker$congress == 106, "R", bycongress.speaker$party)
+
+bycongress.speaker$party_d <- as.numeric(bycongress.speaker$party == "D")
+
+## Create variables: match w/ speaker's party ##
+# 1 = match; 0 = mismatch
+bycongress.speaker$senate_match <- ifelse(bycongress.speaker$senate_control == bycongress.speaker$party, 1, 0)
+bycongress.speaker$house_match <- ifelse(bycongress.speaker$house_control == bycongress.speaker$party, 1, 0)
+bycongress.speaker$pres_match <- ifelse(bycongress.speaker$President == bycongress.speaker$party, 1, 0)
+
+# Dummy code chamber: 0 = H, 1 = S
+bycongress.speaker$chamber_dummy <- ifelse(bycongress.speaker$chamber == 'H', 0, 1)
+
+# Standardize dvs
+for (i in foundations) {
+  bycongress.speaker[[paste(i, "raw", sep = "_")]] <- bycongress.speaker[[i]]
+  bycongress.speaker[[i]] <- scale(bycongress.speaker[[i]], center = TRUE, scale = TRUE)
+}
+
+#### MAIN EFFECTS OF PARTY ####
+
+for (i in foundations)  {
+  cat( paste( "\n############### Overall frequency for:", i, "###############\n") )
+  dv <- bycongress.speaker[[i]]
+  model <- lmer(dv ~ party_d + (1|speaker) + (1|congress), data=bycongress.speaker, REML=TRUE)
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modParty", sep = "."), model )
+}
+
+#### EFFECTS OF PARTY CONTROL ####
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of party control for:", i, "###############\n") )
+  dv <- bycongress.speaker[[i]]
+  model <- lmer(dv ~ senate_match + house_match + pres_match + (1|speaker) + (1|congress), data=bycongress.speaker, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                                               optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modControl", sep = "."), model )
+}
+
+#### SEPARATE MODELS FOR SENATE / HOUSE ####
+# Data frames
+data_senate <- bycongress.speaker %>% filter(chamber == "S")
+data_house <- bycongress.speaker %>% filter(chamber == "H")
+
+## Optimization method changed to nlminb so models will converge
+# Senate
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of party control (Senate members) for:", i, "###############\n") )
+  dv <- data_senate[[i]]
+  model <- lmer(dv ~ party_d + senate_match + (1|speaker) + (1|congress), data=data_senate, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                                    optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modS", sep = "."), model )
+}
+
+# House
+
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of party control (House members) for:", i, "###############\n") )
+  dv <- data_house[[i]]
+  model <- lmer(dv ~ party_d + house_match + (1|speaker) + (1|congress), data=data_house, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                                optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modH", sep = "."), model )
+}
+
+### Models w/ interactions ###
+# Senate
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of party control (Senate members) for:", i, "###############\n") )
+  dv <- data_senate[[i]]
+  model <- lmer(dv ~ party_d * senate_match + (1|speaker) + (1|congress), data=data_senate, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                             optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modS_Int", sep = "."), model )
+}
+
+# House
+
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of party control (House members) for:", i, "###############\n") )
+  dv <- data_house[[i]]
+  model <- lmer(dv ~ party_d * house_match + (1|speaker) + (1|congress), data=data_house, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modH_Int", sep = "."), model )
+}
+
+# Model comparisons
+for (i in foundations) {
+  print(i)
+  print(anova(get(paste(i, "modS", sep = ".")), get(paste(i, "modS_Int", sep = "."))))
+}
+
+for (i in foundations) {
+  print(i)
+  print(anova(get(paste(i, "modH", sep = ".")), get(paste(i, "modH_Int", sep = "."))))
+}
+
+#### Cross-chamber effects ####
+# Senate
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of cross-chamber control (Senate members) for:", i, "###############\n") )
+  dv <- data_senate[[i]]
+  model <- lmer(dv ~ house_match + senate_match + party_d + (1|speaker) + (1|congress), data=data_senate, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                            optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modCS", sep = "."), model )
+}
+
+# House
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of cross-chamber control (House members) for:", i, "###############\n") )
+  dv <- data_house[[i]]
+  model <- lmer(dv ~ senate_match + house_match + party_d + (1|speaker) + (1|congress), data=data_house, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                                          optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modCH", sep = "."), model )
+}
+
+#### Presidency effects ####
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of White House control for:", i, "###############\n") )
+  dv <- bycongress.speaker[[i]]
+  model <- lmer(dv ~ pres_match + (1|speaker) + (1|congress), data=bycongress.speaker, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                                               optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modP", sep = "."), model )
+}
+
+
+#### Effects of single-party governance ####
+# Create variable (108, 109, and 111th Congresses)
+bycongress.speaker$single_party_gov <- ifelse(bycongress.speaker$congress == 108 | bycongress.speaker$congress == 109 | bycongress.speaker$congress == 111, 1, 0)
+
+for (i in foundations)  {
+  cat( paste( "\n############### Effects of single party governance for:", i, "###############\n") )
+  dv <- bycongress.speaker[[i]]
+  model <- lmer(dv ~ single_party_gov + (1|speaker) + (1|congress), data=bycongress.speaker, REML=TRUE, control = lmerControl(optimizer = "optimx", calc.derivs = FALSE,
+                                                                                                                                 optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+  print( summary(model) )
+  
+  # save model
+  assign( paste(i, "modSing", sep = "."), model )
+}
---
+++ Congress/congress_plots.R
@@ -0,0 +1,144 @@
+# This is expected to run after the main analyses. It depends on the data structures created by that script.
+
+library(Rmisc)
+
+### Long form data (used for some plots) ###
+
+data.long <- melt(data, measure.var = c("Fairnessvirtue", "Carevirtue", 
+                                        "Loyaltyvirtue", "Authorityvirtue", 
+                                        "Purityvirtue", "Fairnessvice", 
+                                        "Carevice", "Loyaltyvice", 
+                                        "Authorityvice", "Purityvice"),
+                  variable.name = "foundation",
+                  value.name = "loading",
+                  na.rm = TRUE
+)
+
+### SCATTERPLOT ###
+bycongress.speaker.long <- melt(bycongress.speaker, measure.var = c("Fairnessvirtue", "Carevirtue", 
+                                                                                  "Loyaltyvirtue", "Authorityvirtue", 
+                                                                                  "Purityvirtue", "Fairnessvice", 
+                                                                                  "Carevice", "Loyaltyvice", 
+                                                                                  "Authorityvice", "Purityvice"),
+                                       variable.name = "foundation",
+                                       value.name = "loading",
+                                       na.rm = TRUE
+)
+
+# Reorder columns and define matching labels for the plot
+categoryorder <- c("Carevirtue", "Carevice", "Fairnessvirtue", "Fairnessvice", "Loyaltyvirtue", "Loyaltyvice", 
+                   "Authorityvirtue", "Authorityvice", "Purityvirtue", "Purityvice")
+
+xlabels <- c("Care-virtue", "Care-vice", "Fairness-virtue", "Fairness-vice", "Loyalty-virtue", "Loyalty-vice",
+             "Authority-virtue", "Authority-vice", "Purity-virtue", "Purity-vice")
+
+# this will be used by labeller() below
+foundation_names <- c(
+  `Carevirtue` = "Harm-virtue",
+  `Carevice` = "Harm-vice",
+  `Fairnessvirtue` = "Fairness-virtue",
+  `Fairnessvice` = "Fairness-vice",
+  `Loyaltyvirtue` = "Loyalty-virtue",
+  `Loyaltyvice` = "Loyalty-vice",
+  `Authorityvirtue` = "Authority-virtue",
+  `Authorityvice` = "Authority-vice",
+  `Purityvirtue` = "Purity-virtue",
+  `Purityvice` = "Purity-vice"
+)
+
+scatterplotdata <- arrange(transform(bycongress.speaker.recent.long, foundation=factor(foundation,levels=categoryorder)), foundation)
+
+set.seed(1)
+scatterplotdata$numericfoundation <- as.numeric(scatterplotdata$foundation)
+scatterplotdata$numericfoundation <- jitter(scatterplotdata$numericfoundation)
+
+scatterplotdata$party <- ifelse(scatterplotdata$partyd == 0, "R", "D")
+
+scatterplot <- ggplot(scatterplotdata, aes(x=foundation, y=loading, color=party)) + 
+  geom_jitter(aes(shape=party, colour=party), size = 1) + geom_boxplot(outlier.shape=NA, alpha=0.5)
+
+print( scatterplot + theme_bw() +
+         scale_colour_manual(values=c("royalblue4","#9E2E2E")) + 
+         scale_x_discrete( labels = xlabels ) +
+         labs(y="Mean Similarity") + labs(x="Moral Language Category") + 
+         theme(
+           text = element_text(size=20), 
+           axis.text.x = element_text(angle=45, hjust=1), 
+           panel.grid.major.x = element_blank()
+         ) 
+)
+
+### PARTY CONTROL PLOTS ###
+data.long$chamberd <- ifelse(data.long$chamber == 'H', 0, 1)
+plotdata <- summarySEwithin(data.long, measurevar="loading", betweenvars=c("partyd", "chamberd"), withinvars=c("congress", "foundation"), idvar="speaker")
+
+
+## SENATE ##
+plotdata_senate_97 <- subset(plotdata, chamberd == 1)
+plotdata_senate_97$congress <- as.numeric(levels(plotdata_senate_97$congress))[plotdata_senate_97$congress]
+
+plot_senate <- ggplot(na.omit(plotdata_senate_97), aes(x=congress, y=loading, group=partyd))
+
+plot_senate + facet_wrap(~foundation, nrow=2, ncol=5, labeller = labeller(foundation = as_labeller(foundation_names) ), scales = "free") +
+  geom_rect(aes(xmin=114,
+                xmax = Inf,
+                ymin = -Inf,
+                ymax = Inf), fill = 'firebrick2', alpha = 0.01) +
+  geom_rect(aes(xmin=110,
+                xmax = 114,
+                ymin = -Inf,
+                ymax = Inf), fill = 'lightskyblue1', alpha = 0.01) +
+  geom_rect(aes(xmin=108,
+                xmax = 110,
+                ymin = -Inf,
+                ymax = Inf), fill = 'firebrick2', alpha = 0.01) +
+  geom_rect(aes(xmin=107,
+                xmax = 108,
+                ymin = -Inf,
+                ymax = Inf), fill = 'lightskyblue1', alpha = 0.01) +
+  geom_rect(aes(xmin=104,
+                xmax = 107,
+                ymin = -Inf,
+                ymax = Inf), fill = 'firebrick2', alpha = 0.01) +
+  geom_rect(aes(xmin=100,
+                xmax = 104,
+                ymin = -Inf,
+                ymax = Inf), fill = 'lightskyblue1', alpha = 0.01) +
+  geom_rect(aes(xmin=-Inf,
+                xmax = 100,
+                ymin = -Inf,
+                ymax = Inf), fill = 'firebrick2', alpha = 0.01) +
+  geom_point(size=0.4, aes(colour=partyd)) + geom_errorbar(aes(ymin=loading-se, ymax=loading+se, colour = partyd), width=.1) +
+  geom_line(size=0.3, aes(colour=partyd)) + theme(axis.text.x  = element_text(angle=90, vjust=0.5)) + 
+  scale_colour_manual(values=c("#9E2E2E", "royalblue4"), name="Party", labels = c("Republican", "Democratic")) +
+  xlab("Congress") + ylab("Mean Similarity") + scale_x_continuous(breaks=c(98, 100, 102, 104, 106, 108, 110, 112, 114))
+
+
+## HOUSE ##
+plotdata_house_97 <- subset(plotdata, chamberd == 0)
+plotdata_house_97$congress <- as.numeric(levels(plotdata_house_97$congress))[plotdata_house_97$congress]
+
+
+plot_house <- ggplot(na.omit(plotdata_house_97), aes(x=congress, y=loading, group=partyd))
+
+plot_house + facet_wrap(~foundation, nrow=2, ncol=5, labeller = labeller(foundation = as_labeller(foundation_names) ), scales = "free") +
+  geom_rect(aes(xmin=112,
+                xmax = Inf,
+                ymin = -Inf,
+                ymax = Inf), fill = 'firebrick2', alpha = 0.01) +
+  geom_rect(aes(xmin=110,
+                xmax = 112,
+                ymin = -Inf,
+                ymax = Inf), fill = 'lightskyblue1', alpha = 0.01) +
+  geom_rect(aes(xmin=104,
+                xmax = 110,
+                ymin = -Inf,
+                ymax = Inf), fill = 'firebrick2', alpha = 0.01) +
+  geom_rect(aes(xmin=-Inf,
+                xmax = 104,
+                ymin = -Inf,
+                ymax = Inf), fill = 'lightskyblue1', alpha = 0.01) +
+  geom_point(size=0.4, aes(colour=partyd)) + geom_errorbar(aes(ymin=loading-se, ymax=loading+se, colour = partyd), width=.1) +
+  geom_line(size=0.3, aes(colour=partyd)) + theme(axis.text.x  = element_text(angle=90, vjust=0.5)) + 
+  scale_colour_manual(values=c("#9E2E2E", "royalblue4"), name="Party", labels = c("Republican", "Democratic")) +
+  xlab("Congress") + ylab("Mean Similarity") + scale_x_continuous(breaks=c(98, 100, 102, 104, 106, 108, 110, 112, 114))
